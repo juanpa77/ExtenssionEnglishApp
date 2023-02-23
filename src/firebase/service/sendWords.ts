a7235ecfbc753c2fdbceb055208ac922878934d6
Juan Rodriguez <juanpa.box.77@gmail.com>
2023-02-23 17:22:31 -0300

    new feature// new service to putt array words

--- src/firebase/service/sendWords.ts
+++ src/firebase/service/sendWords.ts
@@ -1,16 +1,22 @@
-import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { arrayUnion, doc, setDoc, updateDoc } from "firebase/firestore";
 import { Action } from "../../background";
 import { db } from "../config";
+import { listWordsWithTranslation } from "./translation";
 
 const sendWordToFirestoreDB = (words: string[]) => {
   chrome.runtime.sendMessage<Action>({ type: 'send-words', payload: words }, /* (response) => {
-    // console.log(response)
   } */);
 }
 
-export const addWordsToFirestoreDb = (uid: string, words: string[]) => {
+export const addWordsToFirestoreDb = async (uid: string, words: string[]) => {
+  const data = await listWordsWithTranslation(words)
   const userRef = doc(db, 'users', uid)
-  updateDoc(userRef, { vocabulary: arrayUnion(...words) });
+  updateDoc(userRef, { vocabulary: arrayUnion(...data) })
+}
+
+export const initFirestoreDb = (uid: string) => {
+  const userRef = doc(db, 'users', uid)
+  setDoc(userRef, { vocabulary: [] });
 }
 
 export default sendWordToFirestoreDB
